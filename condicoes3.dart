a918f1a6cd6881f8f4ce93e51762c9aed67429a5
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-09-22 11:28:43 -0300

    Create condicoes3.dart
    Condição switch case

---
+++ condicoes3.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+  // ===Exemplo 3: switch/ case =========
+
+  stdout.write('Digite uma letra(a, b ou c): ');
+
+  String letra = stdin.readLineSync()!.toLowerCase();
+
+  switch(letra){
+    case 'a':
+    print('Você digitou A. ');
+    break;
+    case 'b':
+    print('Você digitou B.');
+    break;
+    case 'c':
+    print('Você digitou C');
+    break;
+    default:
+    print('Opção iválida');
+  }
+}
