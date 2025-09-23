24062473eb989731a9bd3e31ed82e53e9deaefc8
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-09-23 08:02:36 -0300

    Create maps.dart

---
+++ maps.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<int, String> ddds = {
+    21: "Rio de Janeiro",
+    11: "São Paulo",
+    31: "Belo Horizonte",
+  };
+
+  print(ddds[21]); //Rio de janeiro
+
+  int dddInformado = 11;
+
+  print('DDD $dddInformado = ${ddds[dddInformado]}'); //DDD 11 = São Paulo
+}
