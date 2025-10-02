fd71099edc7e1e9de7127372426fb6d1a88f39c9
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-10-02 13:27:16 -0300

    Update main.dart

--- src/calculo_imc/lib/main.dart
+++ src/calculo_imc/lib/main.dart
@@ -10,12 +10,33 @@ void main() {
 
 class Home extends StatefulWidget{
   const Home ({super.key});
-
+ 
   @override
   State <Home> createState()=> _HomeState();
 }
 
   class _HomeState extends State <Home>{
+      //Controladores para os campos de texto
+    final TextEditingController pesoController = TextEditingController();
+    final TextEditingController alturaController = TextEditingController();
+
+    String _resultado = "";
+    
+    void _calcularIMC(){
+      double peso = double.tryParse(pesoController.text)?? 0;
+      double altura = double.tryParse(alturaController.text)??0;
+      if(peso > 0 && altura > 0){
+        altura = altura /100; //converter cm para m
+        double imc = peso / (altura * altura);
+        setState(() {
+        _resultado = "IMC: ${imc.toStringAsFixed(2)}";
+      });
+      }else{
+        setState((){
+          _resultado = "Preeencha os campos corretamente!";
+        });
+      }
+    }
     @override
     Widget build(BuildContext context){
       return Scaffold(appBar: AppBar(
@@ -57,13 +78,18 @@ class Home extends StatefulWidget{
            ),
            //Botão
            
-           ElevatedButton(onPressed: null,
+           ElevatedButton(onPressed: _calcularIMC, //funcionamentoi do butão
            style: ElevatedButton.styleFrom(
             backgroundColor: Colors.green,
            ),
            child: Text("Calcular",
-           style: TextStyle(color: Colors.white,fontSize: 50))
-           )  
+           style: TextStyle(color: Colors.white,fontSize: 50)),
+           
+           ),
+           SizedBox(height: 20),
+           Text(_resultado,
+           style: TextStyle(fontSize: 25, color: Colors.green),
+           ),  
            ],
       )
       );
