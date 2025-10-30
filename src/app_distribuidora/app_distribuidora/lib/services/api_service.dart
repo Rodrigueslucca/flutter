a3a7d87aa5402127da2afd6ef6ff9e995fda7c90
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-10-30 10:01:05 -0300

    Enseridno informações do banco de dados no app

--- src/app_distribuidora/app_distribuidora/lib/services/api_service.dart
+++ src/app_distribuidora/app_distribuidora/lib/services/api_service.dart
@@ -1,15 +1,9 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-
 import '../models/bebida.dart';
 
 class ApiService {
-  // ⚠️ Se estiver no emulador Android padrão
-  final String baseUrl = "http://10.0.2.2:8089/bebidas";
-
-  // ⚠️ Se for celular físico, troque por IP local do PC
-  // final String baseUrl = "http://192.168.x.x:8089/bebidas";
+  final String baseUrl = "http://localhost:8089/bebidas";
 
   Future<List<Bebida>> getBebidas() async {
     try {
