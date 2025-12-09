7d4e4c655c7534e93514893d8a11cba625db9a5d
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-12-09 11:35:11 -0300

    update

--- src/brick_breaker/lib/main.dart
+++ src/brick_breaker/lib/main.dart
@@ -1,37 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'widgets/crossword_generator_app.dart';             // Add this import
+
 void main() {
   runApp(
     ProviderScope(
       child: MaterialApp(
-        title: 'Crossword builder',
+        title: 'Crossword Builder',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorSchemeSeed: Colors.blueGrey,
           brightness: Brightness.light,
         ),
-        home: Scaffold(
-          body: Center(
-            child: Text('Hello, Word!', style: TextStyle(fontSize: 24)),
-          ),
-          ),
-        ),
-        ),
-  );
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        home: CrosswordGeneratorApp(),                     // Remove what was here and replace
       ),
-    );
-  }
+    ),
+  );
 }
